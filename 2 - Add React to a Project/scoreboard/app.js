04afcda10f14be0b4ffdf6e54ff13ce4d9b297f0
pdxor <kahlilcalavas@gmail.com>
2019-10-09 12:56:32 -0400

    my first react element

---
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -0,0 +1,8 @@
+const title = React.createElement(
+	'h1',
+	{ id: 'main-title', title: 'this is a title' },
+	'My first React Element!'
+);
+
+//<h1 id="main-title" title="This is a title">My first Reacgt Element!</h1>
+console.log(title);
