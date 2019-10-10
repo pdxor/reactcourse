3e7c57a10dd3efbf0ea86484505aa66b4e9cf6c9
pdxor <kahlilcalavas@gmail.com>
2019-10-10 17:08:32 -0400

    fixed sysntax of compionent arrow function

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,12 +1,14 @@
 
 
 //react component use capitol as to differentiate from html markup
-const Header = () => (
+const Header = () =>  {
+	return (
 		<header>
 			<h1>Scoreboard</h1>
 			<span className="stats">Players: 1</span>
 		</header>
-);
+	);
+}
 
 
 
