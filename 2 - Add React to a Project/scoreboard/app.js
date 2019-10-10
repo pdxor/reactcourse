ae8d32079412bbe6130a4a5f84b604ce9d3ec6f7
pdxor <kahlilcalavas@gmail.com>
2019-10-10 12:51:33 -0400

    adding jsx to appjs

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,14 +1,20 @@
-const title = React.createElement(
-	'h1',
-	{ id: 'main-title', title: 'this is a title' },
-	'My first React Element!'
-);
+//jsx
+const title = <h1>My First React Element!</h1>;
 
 //<h1 id="main-title" title="This is a title">My first Reacgt Element!</h1>
 
+const desc = <p>I just learned how to create a Reacte Element and rendered to the DOM.</p>;
+
+
+const header = React.createElement(
+	'header',
+	null,
+	title,
+	desc
+);
 
 ReactDOM.render(
-	title,
+	header,
 	document.getElementById('root')
 
 );
