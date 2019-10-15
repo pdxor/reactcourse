b4f96ff28e38ede7c0f25ff2876472330fe0ca1d
pdxor <kahlilcalavas@gmail.com>
2019-10-15 13:26:38 -0400

    bringing in an array of data

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,4 +1,20 @@
-
+const players = [
+{
+	name: "Kahlil",
+	score: 555,
+	id: 1
+},
+{
+	name: "Jim",
+	score: 111,
+	id: 2
+},
+{
+	name: "jo",
+	score: 222,
+	id: 3
+}
+];
 
 //react component use capitol as to differentiate from html markup
 const Header = (props) =>  {
@@ -11,72 +27,84 @@ const Header = (props) =>  {
 	);
 }
 
-const Player = () => {
+const Player = (props) => {
+	console.log(props);
 	return (
 		<div className="player">
 			<span className="player-name">
-				Kahlil
+				{ props.name }
 			</span>
 		{/* Counter here*/}
-			<Counter />
+			<Counter score={ props.score } />
 		</div>
 		);
 }
-const Counter = () =>  {
+
+const Counter = (props) =>  {
 	return (
 			<div className="counter">
 				<button className="counter-action decrement"> - </button>
-				<span className="counter-score">35</span>
+				<span className="counter-score">{props.score}</span>
 				<button className="counter-action increment"> + </button>
 			</div>
 	);
 }
 
-const App = () => {
+const App = (props) => {
 	return(
 		<div className="scoreboard">
-			<Header title="My Scoreboard" totalPlayers={ n => n + 2}/>
+			<Header 
+				title="My Scoreboard" 
+				totalPlayers={ n => n + 2 }
+			/>
 
 			{/* Player's list */}
-			<Player />
+			{props.initialPlayers.map( player =>
+				<Player 
+					score = {player.score} 
+					name = {player.name} 
+					key = {player.id.toString()}
+				/>
+			)}
+					
 		</div>
 		);
 }
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { seconds: 0 };
-  }
+// class Timer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { seconds: 0 };
+//   }
 
-  tick() {
-    this.setState(state => ({
-      seconds: state.seconds + 1
-    }));
-  }
+//   tick() {
+//     this.setState(state => ({
+//       seconds: state.seconds + 1
+//     }));
+//   }
 
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
+//   componentDidMount() {
+//     this.interval = setInterval(() => this.tick(), 1000);
+//   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.interval);
+//   }
 
-  render() {
-    return (
-      <div>
-        Seconds: {this.state.seconds}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         Seconds: {this.state.seconds}
+//       </div>
+//     );
+//   }
+// }
 
 // ReactDOM.render(
 //   <Timer />,
 //   document.getElementById('timer-example')
 // );
 ReactDOM.render(
-	<App />,
+	<App initialPlayers={players} />,
 	document.getElementById('root')
 
 );
